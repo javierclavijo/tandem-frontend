92c880d2c83b0db25c84bb67e1870934366ec5c1
Javier Clavijo <javierclavijog@gmail.com>
2022-04-02 17:33:36 +0200

    Revert "(WIP) Add Firebase Authentication"
    This reverts commit 457cc4b6bb0ab07a683ac043faa398493cc45f99.

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,18 +1,45 @@
 import React from "react";
-import {StyledFirebaseAuth} from "react-firebaseui";
-import {firebaseConfig, firebaseUiConfig} from "../app/services/firebaseApp";
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-
-
-firebase.initializeApp(firebaseConfig);
+import {useForm} from "react-hook-form";
+import {useNavigate} from "react-router-dom";
+import {LoginRequest, useLoginMutation} from "../app/services/api";
+import {useAppDispatch} from "../app/hooks";
+import {setCredentials} from "../app/authSlice";
 
 function LogIn() {
+    const {register, formState: {errors}, setError, handleSubmit} = useForm<LoginRequest>();
+    const [login] = useLoginMutation();
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    const onSubmit = async (data: LoginRequest) => {
+        try {
+            const response = await login(data).unwrap();
+            dispatch(setCredentials(response));
+            navigate("/chats");
+
+        } catch (exception) {
+            // If login couldn't be performed, display error message
+            setError("password", {
+                type: "server",
+                message: "Incorrect username or password."
+            });
+        }
+    };
 
     return (
         <div>
             <h2>Log In</h2>
-            <StyledFirebaseAuth uiConfig={firebaseUiConfig} firebaseAuth={firebase.auth()}/>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("username",
+                    {required: "Username is required."})}/>
+                {errors.username && errors.username.message}
+
+                <input type="password" {...register("password",
+                    {required: "Password is required"})}/>
+                {errors.password && errors.password.message}
+
+                <button type={"submit"}>Log in</button>
+            </form>
         </div>
     );
 }
