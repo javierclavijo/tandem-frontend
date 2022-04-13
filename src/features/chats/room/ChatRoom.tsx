9369520734b9af11d3a5ed883a10262ba55ac81d
Javier Clavijo <javierclavijog@gmail.com>
2022-04-13 16:33:17 +0200

    Add automatic bottom scrolling to chat

--- src/features/chats/room/ChatRoom.tsx
+++ src/features/chats/room/ChatRoom.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 
-import React, {useState} from "react";
+import React, {SyntheticEvent, useState} from "react";
 import {useParams} from "react-router-dom";
 import {messageSortFn, useChatList} from "../hooks";
 import {Chat} from "../../../entities/Chat";
@@ -18,23 +18,50 @@ function ChatRoom() {
     const {data: chatList} = useChatList();
     const [chat, setChat] = useState<Chat>({} as Chat);
 
+    const messageContainerRef = React.useRef<HTMLDivElement>(null);
+    const [isScrollBottom, setIsScrollBottom] = React.useState<boolean>(true);
+
     const {data} = useQuery<Chat>(["chats", "detail", chat.id], async () => {
         const response = await axiosApi.get(chat.url);
         return response.data;
     }, {
         enabled: Boolean(chat.id),
         staleTime: 15000,
-        // Sort messages whenever query data changes
-        onSuccess: (data) => data.messages.sort((a, b) => messageSortFn(a, b)).reverse(),
+        // Whenever query data changes, scroll to the bottom of the chat if it's positioned there to show new
+        // messages, then sort messages
+        onSuccess: (data) => {
+            scrollToBottom();
+            return data.messages.sort((a, b) => messageSortFn(a, b)).reverse();
+        },
     });
 
-    React.useEffect(() => {
+    React.useLayoutEffect(() => {
         // Fetch the resource's URL and ID from the chat list
         const chatResult = chatList?.find(c => c.id === params.id);
         if (chatResult) {
             setChat(chatResult);
         }
     }, [chatList, params.id]);
+
+    const scrollToBottom = () => {
+        if (isScrollBottom) {
+            messageContainerRef.current?.scrollTo(0, messageContainerRef.current.scrollHeight);
+        }
+    };
+
+    React.useEffect(scrollToBottom, [data?.messages, isScrollBottom]);
+
+    const handleScroll = (event: SyntheticEvent) => {
+        // On scroll, detect if the user has scrolled to bottom and set isScrollBottomRef.current accordingly. A number
+        // of px are subtracted to have a bit of margin
+        const element = event.target as HTMLDivElement;
+        setIsScrollBottom(element.offsetHeight + element.scrollTop >= element.scrollHeight - 100);
+    };
+
+    React.useEffect(() => {
+        // When another chat is selected, set the property to scroll to the bottom again
+        setIsScrollBottom(true);
+    }, [chat]);
 
     return (
         <div css={css`
@@ -56,12 +83,14 @@ function ChatRoom() {
             `}>
                 <h2>{chat.name}</h2>
             </header>
-            <div css={css`
-              overflow-y: scroll;
-              height: 100%;
-              display: flex;
-              flex-direction: column;
-            `}>
+            <div ref={messageContainerRef}
+                 onScroll={handleScroll}
+                 css={css`
+                   overflow-y: scroll;
+                   height: 100%;
+                   display: flex;
+                   flex-direction: column;
+                 `}>
                 {data?.messages.map(message => (
                     <ChatRoomMessage message={message}
                                      isOwnMessage={user?.id === message.author.id}
