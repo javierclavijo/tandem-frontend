db4c4b711a8c3237351a7b1d92dec4a3e77bd60b
Javier Clavijo <javierclavijog@gmail.com>
2022-04-23 19:42:10 +0200

    Add edition functionality to NameInput

--- src/features/info/components/NameInput.tsx
+++ src/features/info/components/NameInput.tsx
@@ -7,21 +7,25 @@ import {css} from "@emotion/react";
 import {colors} from "../../../styles/variables";
 import {editElement} from "../channel/styles";
 import EditButtons from "./EditButtons";
+import {axiosApi} from "../../auth/AuthContext";
+import {useMutation, useQueryClient} from "react-query";
 
 interface NameInputProps {
     data: Channel | undefined;
 }
 
 interface NameInputRequestData {
-    description: string;
+    name: string;
 }
 
 function NameInput({data}: NameInputProps) {
 
+    const queryClient = useQueryClient();
+
     const {
         editEnabled, setEditEnabled,
-        value, setValue,
-        error, setError, clearError,
+        value,
+        error, setError,
         elementRef,
         submitButtonRef,
         handleChange,
@@ -30,16 +34,63 @@ function NameInput({data}: NameInputProps) {
     } = useEdit<HTMLInputElement>(data, "name");
 
 
+    const updateRequest = async (requestData: NameInputRequestData) => {
+        const response = await axiosApi.patch(`/channels/${data?.id}/`, requestData);
+        return response.data;
+    };
+
+    const updateMutation = useMutation(updateRequest, {
+        onSuccess: async (requestData) => {
+            queryClient.setQueryData<Channel | undefined>(["chats", "info", data?.id], (old) => {
+                if (old) {
+                    old.name = requestData.name;
+                }
+                return old;
+            });
+        }
+    });
+
+
+    const handleBlur = async (event: React.FocusEvent<HTMLInputElement>) => {
+        // If the submit button was clicked, submit the value. Else, cancel the editing.
+        if (event.relatedTarget === submitButtonRef?.current) {
+            const success = await handleSubmit();
+            if (!success) {
+                elementRef?.current?.focus();
+            }
+        } else {
+            handleCancel();
+        }
+    };
+
     const handleSubmit = async () => {
         if (!value) {
             setError("Description must have a length between 1 and 2000 characters.");
             return false;
         }
-        const requestData = {description: value};
-        // await updateMutation.mutateAsync(requestData);
+        const requestData = {name: value};
+        await updateMutation.mutateAsync(requestData);
         setEditEnabled(false);
         return true;
     };
+
+    const handleKeyDown = async (event: React.KeyboardEvent<HTMLInputElement>) => {
+        // If the Meta or Control key is pressed at the same time as the Enter key, submit the form. If the pressed key
+        // is the Escape key, cancel the editing and blur the text area.
+        if (event.code === "Enter") {
+            const success = await handleSubmit();
+            if (success) {
+                elementRef.current?.blur();
+            } else {
+                event.preventDefault();
+            }
+
+        } else if (event.code === "Escape") {
+            handleCancel();
+            elementRef.current?.blur();
+        }
+    };
+
 
     return (
         <div css={css`
@@ -49,12 +100,14 @@ function NameInput({data}: NameInputProps) {
           align-items: center;
         `}>
             <input type="text" id="name" name="name" ref={elementRef}
-                   css={css`${editElement};
-                     text-align: center;
-                   `}
                    value={value}
                    onChange={handleChange}
                    onFocus={handleFocus}
+                   onBlur={handleBlur}
+                   onKeyDown={handleKeyDown}
+                   css={css`${editElement};
+                     text-align: center;
+                   `}
             />
             <EditButtons editEnabled={editEnabled} submitButtonRef={submitButtonRef}
                          handleSubmit={handleSubmit} handleCancel={handleCancel}
