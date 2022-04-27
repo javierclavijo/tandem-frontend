0462e9299197dd13798ccaca240826ac8d00a946
Javier Clavijo <javier.clavijo@aktios.com>
2022-04-27 14:12:29 +0200

    Add language edit badge to channel info view

---
+++ src/features/info/channel/ChannelEditLanguageBadge.tsx
@@ -0,0 +1,87 @@
+/** @jsxImportSource @emotion/react */
+
+import React from "react";
+import {css} from "@emotion/react";
+import {FlagIcon} from "react-flag-kit";
+import {ThreeStars} from "iconoir-react";
+import {colors} from "../../../styles/variables";
+import {flagCodes} from "../../../resources/languages";
+import {badge, badgeSelect} from "../../../styles/components";
+import {UserLanguage} from "../../../entities/User";
+import {languageOptions, levelOptions, Option} from "../components/InfoSelect";
+import {useMutation, useQueryClient} from "react-query";
+import {axiosApi} from "../../auth/AuthContext";
+import Select from "react-select";
+import {Channel} from "../../../entities/Channel";
+
+interface LanguageBadgeProps {
+    data: UserLanguage;
+    bg: string;
+}
+
+function UserInfoEditLanguageBadge({data, bg}: LanguageBadgeProps) {
+
+    const queryClient = useQueryClient();
+    const [languageValue, setLanguageValue] = React.useState<Option | null>(null);
+    const [levelValue, setLevelValue] = React.useState<Option | null>(null);
+
+
+    const updateRequest = React.useCallback(async (requestData: { language: string }) => {
+        if (data) {
+            const response = await axiosApi.patch(data?.url, requestData);
+            return response.data;
+        }
+    }, [data]);
+
+    const updateMutation = useMutation(updateRequest, {
+        onSuccess: async () => {
+            await queryClient.invalidateQueries<Channel | undefined>(["chats", "info", data?.id]);
+        }
+    });
+
+    const handleChange = async (option: Option, key: keyof Channel) => {
+        const requestData = {} as any;
+        requestData[key] = option.value;
+        await updateMutation.mutateAsync(requestData);
+    };
+
+    React.useEffect(() => {
+        // Get the options which correspond to the data values and set them as the selects' values
+        const initialLevelOption = levelOptions.find(o => o.value === data.level);
+        const initialLanguageOption = languageOptions.find(o => o.value === data.language);
+        if (initialLevelOption && initialLanguageOption) {
+            setLevelValue(initialLevelOption);
+            setLanguageValue(initialLanguageOption);
+        }
+    }, []);
+
+
+    return (
+        <div css={css`${badge};
+          background-color: ${bg}`}>
+            <FlagIcon code={flagCodes.find(x => x.key === data.language)?.value || "AD"} size={24}/>
+            <Select id={`language-${data.id}`}
+                    value={languageValue}
+                    onChange={async (option: any) => {
+                        setLanguageValue(option);
+                        await handleChange(option, "language");
+                    }}
+                    options={languageOptions}
+                    styles={badgeSelect}
+            />
+            <span>|</span>
+            <ThreeStars color={colors.WHITE} height={24} width={24}/>
+            <Select id={`level-${data.id}`}
+                    value={levelValue}
+                    onChange={async (option: any) => {
+                        setLevelValue(option);
+                        await handleChange(option, "level");
+                    }}
+                    options={levelOptions}
+                    styles={badgeSelect}
+            />
+        </div>
+    );
+}
+
+export default UserInfoEditLanguageBadge;
--- src/features/info/channel/ChannelInfo.tsx
+++ src/features/info/channel/ChannelInfo.tsx
@@ -5,17 +5,17 @@ import {Chat} from "../../../entities/Chat";
 import {chatRoomCss, chatRoomCssMobile} from "../../chats/room/styles";
 import ChatRoomHeader from "../../chats/room/ChatRoomHeader";
 import {useMediaQuery} from "react-responsive";
-import {useMutation, useQuery, useQueryClient} from "react-query";
+import {useQuery} from "react-query";
 import {Channel} from "../../../entities/Channel";
 import useAuth, {axiosApi} from "../../auth/AuthContext";
 import {css} from "@emotion/react";
 import DescriptionTextarea from "../components/DescriptionTextarea";
-import {descriptionSection, infoSection, languageItem, listSection, profileImg} from "./styles";
+import {descriptionSection, infoSection, listSection, profileImg} from "./styles";
 import InfoListElement from "./InfoListElement";
 import {ChannelNameInput} from "../components/NameInput";
-import InfoSelect, {languageOptions, levelOptions, Option} from "../components/InfoSelect";
 import LanguageBadge from "../../../components/LanguageBadge/LanguageBadge";
 import {colors} from "../../../styles/variables";
+import ChannelEditLanguageBadge from "./ChannelEditLanguageBadge";
 
 const placeholderImg = require("../../../static/images/user_placeholder.png");
 
@@ -23,7 +23,6 @@ const placeholderImg = require("../../../static/images/user_placeholder.png");
 function ChannelInfo({chat}: { chat: Chat }) {
 
     const isDesktop = useMediaQuery({query: "(min-width: 1024px)"});
-    const queryClient = useQueryClient();
     const {user} = useAuth();
     const {data} = useQuery<Channel>(["chats", "info", chat.id], async () => {
         const response = await axiosApi.get(chat.info_url);
@@ -42,25 +41,6 @@ function ChannelInfo({chat}: { chat: Chat }) {
         )), [data?.memberships, user]);
 
 
-    const languageUpdateRequest = React.useCallback(async (requestData: { language: string }) => {
-        if (data) {
-            const response = await axiosApi.patch(data?.url, requestData);
-            return response.data;
-        }
-    }, [data]);
-
-    const languageUpdateMutation = useMutation(languageUpdateRequest, {
-        onSuccess: async () => {
-            await queryClient.invalidateQueries<Channel | undefined>(["chats", "info", data?.id]);
-        }
-    });
-
-    const handleChange = async (option: Option, key: keyof Channel) => {
-        const requestData = {} as any;
-        requestData[key] = option.value;
-        await languageUpdateMutation.mutateAsync(requestData);
-    };
-
     return data ?
         <div css={css`${isDesktop ? chatRoomCss : chatRoomCssMobile};
           overflow-y: scroll;
@@ -75,7 +55,10 @@ function ChannelInfo({chat}: { chat: Chat }) {
                     <p>{data?.name}</p>
                 }
                 <p>Channel · {data?.memberships.length} members</p>
-                <LanguageBadge language={data.language} level={data.level} bg={colors.DARK}/>
+                {editable ?
+                    <ChannelEditLanguageBadge data={data} bg={colors.DARK}/> :
+                    <LanguageBadge language={data.language} level={data.level} bg={colors.DARK}/>
+                }
                 <section css={descriptionSection}>
                     {editable && data ?
                         <DescriptionTextarea data={data}/> :
@@ -85,31 +68,6 @@ function ChannelInfo({chat}: { chat: Chat }) {
                         </React.Fragment>
                     }
                 </section>
-                {editable && data ?
-                    <section css={css`
-                      width: 100%;
-                      display: flex;
-                      gap: 0.5rem;
-                    `}>
-                        <div css={languageItem}>
-                            <label htmlFor="language">Language</label>
-                            <InfoSelect id="language"
-                                        initialValue={data.language}
-                                        options={languageOptions}
-                                        handleSubmit={(option) => handleChange(option, "language")}
-                            />
-                        </div>
-                        <div css={languageItem}>
-                            <label htmlFor="level">Level</label>
-                            <InfoSelect id="level"
-                                        initialValue={data.level}
-                                        options={levelOptions}
-                                        handleSubmit={(option) => handleChange(option, "level")}
-                            />
-                        </div>
-                    </section> :
-                    null
-                }
             </section>
             <section css={listSection}>
                 <h3>Members</h3>
