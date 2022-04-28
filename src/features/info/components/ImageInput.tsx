53b66ed72314dbe7d34d71f818b07ad9fef5e368
Javier Clavijo <javier.clavijo@aktios.com>
2022-04-28 11:48:40 +0200

    Add image input hover label

--- src/features/info/components/ImageInput.tsx
+++ src/features/info/components/ImageInput.tsx
@@ -5,6 +5,7 @@ import {profileImg} from "../styles";
 import {useMutation, useQueryClient} from "react-query";
 import {axiosApi} from "../../auth/AuthContext";
 import {css} from "@emotion/react";
+import {colors, textSizes} from "../../../styles/variables";
 
 interface ImageInputProps {
     image: string,
@@ -17,6 +18,10 @@ function ImageInput({image, defaultImage, url, invalidateQueryKey}: ImageInputPr
 
     const queryClient = useQueryClient();
     const inputRef = React.useRef<HTMLInputElement | null>(null);
+
+    // State which controls the visibility of the label. Activated on hover (:hover would not work, as the label needs
+    // to be on top of the input for that, which causes image dropping to not work)
+    const [isLabelDisplayed, setIsLabelDisplayed] = React.useState<boolean>(false);
 
     const request = async (data: FormData) => {
         const response = await axiosApi.patch(url, data, {
@@ -43,14 +48,17 @@ function ImageInput({image, defaultImage, url, invalidateQueryKey}: ImageInputPr
     };
 
     return (
-        <div css={css`
-          display: grid;
-          grid-template-columns: 1fr;
-          grid-template-rows: 1fr;
-          grid-template-areas: "input";
-          width: 12rem;
-          height: 12rem;
-        `}>
+        <div
+            onMouseEnter={() => setIsLabelDisplayed(true)}
+            onMouseLeave={() => setIsLabelDisplayed(false)}
+            css={css`
+              display: grid;
+              grid-template-columns: 1fr;
+              grid-template-rows: 1fr;
+              grid-template-areas: "input";
+              width: 12rem;
+              height: 12rem;
+            `}>
             <input type="file" name="image" id="image"
                    accept="image/jpeg,image/png,image/webp"
                    onChange={handleChange}
@@ -63,6 +71,26 @@ function ImageInput({image, defaultImage, url, invalidateQueryKey}: ImageInputPr
                    `}
                    ref={inputRef}
             />
+            <label htmlFor={"image"}
+                   css={css`
+                     grid-area: input;
+                     display: flex;
+                     justify-content: center;
+                     align-items: center;
+                     text-align: center;
+                     z-index: 5;
+                     height: 100%;
+                     width: 100%;
+                     background-color: ${colors.LIGHT}80;
+                     border-radius: 50%;
+                     -webkit-text-stroke: 1px black;
+                     font-size: ${textSizes.L};
+                     backdrop-filter: blur(2px);
+                     // Set the opacity of the element
+                     opacity: ${isLabelDisplayed ? 1 : 0};
+                   `}>
+                Click or drop a picture here...
+            </label>
             <img src={image ?? defaultImage} alt="" css={css`${profileImg};
               grid-area: input;
             `}/>
