35d8fab93cb74bb962c86d5f3c76321d5fa8ffb8
Javier Clavijo <javierclavijog@gmail.com>
2022-05-05 19:02:49 +0200

    Fix bug on chat list elements order

--- src/features/chats/list/ChatListElements.tsx
+++ src/features/chats/list/ChatListElements.tsx
@@ -5,6 +5,7 @@ import {listElementContainerCss} from "../styles";
 import ChatListElement from "./ChatListElement";
 import {Chat} from "../../../entities/Chat";
 import {css} from "@emotion/react";
+import {messageSortFn} from "../hooks";
 
 interface ChatListElementsProps {
     data: Chat[];
@@ -27,13 +28,15 @@ function ChatListElements({data, filter, userId, selectedId}: ChatListElementsPr
 
     return (
         <div css={listElementContainerCss}>
-            {filteredElements.map(chat =>
-                <ChatListElement chat={chat}
-                                 selected={chat.id === selectedId}
-                                 latestMessageIsOwn={chat.messages[0]?.author.id === userId}
-                                 key={chat.id}
-                />
-            )}
+            {/* Sort chats according to their latest messages' sent datetime. */}
+            {filteredElements
+                .sort((a, b) => messageSortFn(a.messages[0], b.messages[0]))
+                .map(chat => <ChatListElement chat={chat}
+                                              selected={chat.id === selectedId}
+                                              latestMessageIsOwn={chat.messages[0]?.author.id === userId}
+                                              key={chat.id}
+                    />
+                )}
             {/* Empty list. Has different strings for cases where the user has chats and is filtering, and where the
             user doesn't have any chats. */}
             {!filteredElements.length ?
