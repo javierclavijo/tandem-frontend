ba2cc1f6bc1cca9f37c13f984ec9a356d7bc356c
Javier Clavijo <javier.clavijo@aktios.com>
2022-05-17 07:40:15 +0000

    Add styles to header logout button

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,6 +6,7 @@ import useAuth from "../features/auth/AuthContext";
 import {css} from "@emotion/react";
 import {colors, textSizes} from "../styles/variables";
 import {useMediaQuery} from "react-responsive";
+import { buttonWithoutBackgroundAndBorder } from "./Button";
 
 const defaultImg = require("../static/images/user_placeholder.png");
 
@@ -30,7 +31,15 @@ function Nav() {
                             <li><NavLink to={"/chats"} css={linkCss}>Chats</NavLink></li>
                             <li><NavLink to={`/chats/users/${user?.id}`} css={linkCss}>{user?.username}</NavLink></li>
                             <li><NavLink to={"/search"} css={linkCss}>Search</NavLink></li>
-                            <li><button type="button" onClick={handleLogout} css={linkCss}>Log out</button></li>
+                            <li>
+                              <button type="button" onClick={handleLogout} 
+                                css={css`${buttonWithoutBackgroundAndBorder};
+                                          color: ${colors.WHITE};
+                                          font-size: ${textSizes.M};
+                                `}>
+                                Log out
+                              </button>
+                              </li>
                             <li>
                                 <div css={imageContainer}>
                                     <img src={user?.image ?? defaultImg} alt={user?.username}
@@ -42,7 +51,15 @@ function Nav() {
                         <React.Fragment>
                             <li><NavLink to={"/login"} css={linkCss}>Log in</NavLink></li>
                             <li><NavLink to={"/register"} css={linkCss}>Sign in</NavLink></li>
-                            <li><button type="button" onClick={handleLogout} css={linkCss}>Log out</button></li>
+                            <li>
+                            <button type="button" onClick={handleLogout} 
+                                css={css`${buttonWithoutBackgroundAndBorder};
+                                          color: ${colors.WHITE};
+                                          font-size: ${textSizes.M};
+                                `}>
+                                Log out
+                              </button>
+                            </li>
                         </React.Fragment>
                     }
                 </ul>
